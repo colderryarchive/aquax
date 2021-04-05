80b681ffcc5d12f5bead8b3906f7be2850996ffa
Colderry <72723052+Colderry@users.noreply.github.com>
2021-04-05 23:03:44 +0600

    Main file

---
+++ aquax.js
@@ -0,0 +1,17 @@
+module.exports = {
+    Anime: {
+        hug: require("./src/Anime/hug.js"),
+        waifu: require("./src/Anime/waifu.js"),
+        cuddle: require("./src/Anime/cuddle.js"),
+        tickle: require("./src/Anime/tickle.js"),
+        wallpaper: require("./src/Anime/wallpaper.js"),
+        fact: require("./src/Anime/fact.js"),
+        pat: require("./src/Anime/pat.js")
+    },
+    Random: {
+        eightball: require("./src/Random/8ball.js")
+    },
+    Search: {
+        npm: require("./src/Search/npm.js")
+    }
+}
